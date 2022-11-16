4f4bdfeecfd06f09339fc5550f9b68337a73abc0
vandu1979 <vandana_kothari@yahoo.com>
2022-11-15 17:05:29 -0700

    Hi

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.engine('jsx', require('express-react-views').createEngine());
 
 // =====Middleware=====//
 //const fruits = ['apple', 'banana', 'pear'];
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+app.use(express.urlencoded({extended:false}));
 
 //-----Routes------//
 //I.N.D.U.C.E.S.
@@ -19,16 +24,34 @@ app.engine('jsx', require('express-react-views').createEngine());
 
 // Index
 app.get('/fruits', (req, res) => {
-    res.send(fruits);
+    res.render('Index', {
+      fruits: fruits  
+    });
 });
 
 // New
-
+app.get('/fruits/new', (req, res)=>{
+    res.render('New');
+})
 // Delete
 
 // Update
 
 // Create
+// app.post("/fruits", (req, res)=>{
+//     console.log(req.body)
+//     res.render("Data received");
+// });
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.redirect('/fruits');
+});
 
 // Edit
 
@@ -36,8 +59,11 @@ app.get('/fruits', (req, res) => {
 // app.get('/fruits/:Index', (req, res) => {
 //     res.send(fruits[req.params.Index]);
 // });
-app.get('/fruits/:indexOfFruitsArray', function(req, res){
-    res.render('Show');
+app.get('/fruits/:index', function(req, res) {
+    res.render('Show', {
+        fruit: fruits[req.params.index]
+
+    });
 });
 
 
